4e82693008929a4be7c4d276d116618024d6309b
Harish <harishveerapoor6@gmail.com>
2025-08-22 16:39:28 +0530

    second

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -3,196 +3,108 @@ package testBase;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.Duration;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Parameters;
-
-import com.beust.jcommander.Parameter;
-
-import org.apache.logging.log4j.Logger;  // this important one we need import for logger 
-import org.apache.logging.log4j.LogManager;// these also important
-
-
 
 public class BaseClass {
 
-public static WebDriver driver;
-// part 2-> we using here logger
-public Logger logger;
-// part 3-> property class
-public Properties p;
-	
-	@BeforeClass(groups= {"Sanity","Regreesion","Master"})
-	@Parameters({"os","browser"})
-	public void setup(String os, String br) throws IOException {
-		
-		// part -4] property class
-		
-		FileReader file=new FileReader("./src//test//resourcess//config.properties");
-		p=new Properties();
-		p.load(file);
-		
-		// part -2]
-	    logger=LogManager.getLogger(this.getClass());  // the logg method help to see what are the class are exicuted and storing each record
-		                                         // after this we nedd to go for TC_AccountRegisterationTest.java class see their
-	    
-	    
-	    
-	    // part - 10 selenium grid
-	    
-	    if(p.getProperty("execution_env").equals("remote")) {
-	    	
-	    	DesiredCapabilities capbale=new DesiredCapabilities();
-	    	
-	    	// os
-	    	
-	    	if(os.equalsIgnoreCase("window")) {
-	    		capbale.setPlatform(Platform.WIN11);
-	    	}
-	    	else if(os.equalsIgnoreCase("mac")) {
-	    		
-	    		capbale.setPlatform(Platform.MAC);
-	    	}
-	    	else {
-	    		System.out.println("no matching os");
-	    		return;
-	    	}
-	    	
-	    	switch(br.toLowerCase()) {
-	    	
-	    	case "chrome":capbale.setBrowserName("chrome");break;
-	    	case "firefox":capbale.setBrowserName("firefox");break;
-	    	default:System.out.println("no browsing");break;
-	    	}
-	    	
-	    	driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),capbale);
-	    }
-	    
-	    // these is for for opening a browser headed mode
-	    
-//	    if(p.getProperty("execution_env").equals("local")) {
-//	    
-//	    	// Part -3]
-//		    switch(br.toLowerCase()) {
-//		    
-//		    case "chrome" : driver=new ChromeDriver();break;
-//		    case "edge" : driver=new EdgeDriver();break;
-//		    case "firefox" : driver=new FirefoxDriver();break;
-//		    default : System.out.println("Invalid browser name...");return;
-//	    
-//	    }
-	  
-	    // this is for not opening broweser  =headless mode
-	    
-		    if (p.getProperty("execution_env").equals("local")) {
-		        switch (br.toLowerCase()) {
+    public static WebDriver driver;
+    public Properties p;
+    public Logger logger;   // 🔹 Logger variable
 
-		            case "chrome":
-		                ChromeOptions chromeOptions = new ChromeOptions();
-		                chromeOptions.addArguments("--headless");
-		                chromeOptions.addArguments("--no-sandbox");
-		                chromeOptions.addArguments("--disable-dev-shm-usage");
-		                driver = new ChromeDriver(chromeOptions);
-		                break;
+    @BeforeClass
+    public void setUp() throws IOException {
+        // Initialize logger
+        logger = LogManager.getLogger(this.getClass());
 
-		            case "firefox":
-		                FirefoxOptions firefoxOptions = new FirefoxOptions();
-		                firefoxOptions.addArguments("--headless");
-		                driver = new FirefoxDriver(firefoxOptions);
-		                break;
+        // Load config.properties
+        FileReader file = new FileReader("./src/test/resources/config.properties");
+        p = new Properties();
+        p.load(file);
 
-		            case "edge":
-		                EdgeOptions edgeOptions = new EdgeOptions();
-		                edgeOptions.addArguments("--headless=new");
-		                driver = new EdgeDriver(edgeOptions);
-		                break;
+        String env = p.getProperty("execution_env", "local").toLowerCase();
 
-		            default:
-		                System.out.println("Invalid browser name...");
-		                return;
-		        }
-		    }
+        if (env.equals("local")) {
+            logger.info("Execution Environment: Local");
+            driver = new ChromeDriver();   // local will run normal Chrome
+        } 
+        else if (env.equals("remote")) {
+            logger.info("Execution Environment: Remote (Headless)");
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            driver = new ChromeDriver(options);
+        } 
+        else if (env.equals("firefox")) {
+            driver = new FirefoxDriver();
+        } 
+        else if (env.equals("edge")) {
+            driver = new EdgeDriver();
+        } 
+        else {
+            logger.error("Invalid browser/exec_env in config.properties");
+            throw new RuntimeException("Browser not supported: " + env);
+        }
 
-	    
-	    
-	    
-	    // Part -3]
-//	    switch(br.toLowerCase()) {
-//	    
-//	    case "chrome" : driver=new ChromeDriver();break;
-//	    case "edge" : driver=new EdgeDriver();break;
-//	    case "firefox" : driver=new FirefoxDriver();break;
-//	    default : System.out.println("Invalid browser name...");return;
-//	    } // excuite in master.xml 
-		
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().implicitlyWait(50,TimeUnit.MILLISECONDS);
-		
-		driver.get(p.getProperty("URL"));  // reading from property file
-		driver.manage().window().maximize();
-		
-		
-	}
-	
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get(p.getProperty("URL"));
 
-	
-	@AfterClass(groups= {"Sanity","Regreesion","Master"})
-	public void tearDown() {
-		
-		driver.quit();
-	}
-	public String randomString() {
-		String genaratedString=RandomStringUtils.randomAlphabetic(5);
-		return genaratedString;
-		
-	}
-	
-	public String randomNumber() {
-		String genaratedNumaber=RandomStringUtils.randomAlphanumeric(10);
-		return genaratedNumaber;
-		
-	}
-	public String randomAphaNumaric() {
-		String genaratedString=RandomStringUtils.randomAlphabetic(5);
-		String genaratedNumaber=RandomStringUtils.randomAlphanumeric(10);
-		return (genaratedString+genaratedNumaber);
-		
-	}
-	
-	
-	// part 8
-	  public String captureScreen(String tname) throws IOException {
-	        
-	        String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
-	        
-	        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
-	        File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        logger.info("URL launched: " + p.getProperty("URL"));
+    }
 
-	        String targetFilePath = System.getProperty("user.dir") + "\\screenshots\\" + tname + "_" + timeStamp + ".png";
-	        File targetFile = new File(targetFilePath);
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+            logger.info("Browser closed successfully");
+        }
+    }
 
-	        // Copy screenshot to destination
-	        sourceFile.renameTo(targetFile);
+    // Generate random string
+    public String randomString() {
+        String generatedString = RandomStringUtils.randomAlphabetic(5);
+        return generatedString;
+    }
 
-	        return targetFilePath;
-	    }
+    // Generate random number (10-digit alphanumeric)
+    public String randomNumber() {
+        String generatedNumber = RandomStringUtils.randomAlphanumeric(10);
+        return generatedNumber;
+    }
+
+    // Generate random Alpha + Numeric combination
+    public String randomAphaNumaric() {
+        String generatedString = RandomStringUtils.randomAlphabetic(5);
+        String generatedNumber = RandomStringUtils.randomNumeric(3);
+        return (generatedString + generatedNumber);
+    }
+
+    // Capture Screenshot
+    public String captureScreen(String tname) throws IOException {
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
+
+        String targetPath = System.getProperty("user.dir") + "/screenshots/" + tname + ".png";
+        File target = new File(targetPath);
+        FileUtils.copyFile(source, target);
+
+        logger.info("Screenshot captured: " + targetPath);
+        return targetPath;
+    }
 }
