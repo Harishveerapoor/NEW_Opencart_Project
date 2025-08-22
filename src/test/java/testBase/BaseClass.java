d1799210bd49f00bf6b48bf4ae2f0d6d46a51b9f
HAREESH VEERAPOOR <144092935+Harishveerapoor@users.noreply.github.com>
2025-08-22 19:01:37 +0530

    Update BaseClass.java

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -3,115 +3,88 @@ package testBase;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseClass {
 
     public static WebDriver driver;
-    public Logger logger; // Logger variable
-    public Properties p; // Properties variable
+    public Logger logger;
+    public Properties p;
 
-    @BeforeClass(groups = {"Sanity", "Regreesion", "Master"})
-    @Parameters({"os", "browser"})
-    public void setup(String os, String br) throws IOException {
+    @BeforeClass(groups = { "Sanity", "Regression", "Master" })
+    @Parameters({ "browser" })
+    public void setup(String br) throws IOException {
 
-        // Initialize logger first
+        // Initialize Logger
         logger = LogManager.getLogger(this.getClass());
 
         // Load config.properties
-        FileReader file=new FileReader("./src//test//resourcess//config.properties");// Fixed path
+        FileReader file = new FileReader("./src//test//resourcess//config.properties");
         p = new Properties();
         p.load(file);
 
-        // Selenium Grid or local execution
-        if (p.getProperty("execution_env").equalsIgnoreCase("remote")) {
+        logger.info("Execution Environment: " + p.getProperty("execution_env"));
 
-            DesiredCapabilities capbale = new DesiredCapabilities();
-
-            // OS selection
-            if (os.equalsIgnoreCase("window")) {
-                capbale.setPlatform(Platform.WIN11);
-            } else if (os.equalsIgnoreCase("mac")) {
-                capbale.setPlatform(Platform.MAC);
-            } else {
-                logger.error("No matching OS found for Selenium Grid");
-                return;
-            }
-
-            // Browser selection
+        // Local execution
+        if (p.getProperty("execution_env").equalsIgnoreCase("local")) {
             switch (br.toLowerCase()) {
                 case "chrome":
-                    capbale.setBrowserName("chrome");
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--no-sandbox");
+                    options.addArguments("--disable-dev-shm-usage");
+                    driver = new ChromeDriver(options);
                     break;
+
                 case "firefox":
-                    capbale.setBrowserName("firefox");
-                    break;
-                default:
-                    logger.error("No matching browser found for Selenium Grid");
-                    break;
-            }
-
-            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capbale);
-
-        } else if (p.getProperty("execution_env").equalsIgnoreCase("local")) {
-
-            // Local browser selection
-            switch (br.toLowerCase()) {
-                case "chrome":
-                    driver = new ChromeDriver();
-                    break;
-                case "edge":
-                    driver = new EdgeDriver();
-                    break;
-                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
                     break;
-                default:
-                    logger.error("Invalid browser name provided");
-                    return;
-            }
 
-            // Optional headless Chrome
-            if (br.equalsIgnoreCase("chrome")) {
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("--headless");
-                options.addArguments("--no-sandbox");
-                options.addArguments("--disable-dev-shm-usage");
-                driver = new ChromeDriver(options); // Only applies if you want headless Chrome
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+
+                default:
+                    logger.error("Invalid browser name in master.xml: " + br);
+                    throw new RuntimeException("Browser not supported: " + br);
             }
+        } else {
+            logger.error("Currently only 'local' execution_env is supported.");
+            throw new RuntimeException("Unsupported execution_env: " + p.getProperty("execution_env"));
         }
 
+        // Browser setup
         driver.manage().deleteAllCookies();
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.MILLISECONDS);
-        driver.get(p.getProperty("URL"));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
+        // Open URL from config
+        driver.get(p.getProperty("URL"));
         logger.info("URL launched: " + p.getProperty("URL"));
     }
 
-    @AfterClass(groups = {"Sanity", "Regreesion", "Master"})
+    @AfterClass(groups = { "Sanity", "Regression", "Master" })
     public void tearDown() {
         if (driver != null) {
             driver.quit();
@@ -119,7 +92,7 @@ public class BaseClass {
         }
     }
 
-    // ---------------- Random Methods (UNCHANGED) ----------------
+    // --------------- Random Generators (Do not change) ---------------
     public String randomString() {
         String genaratedString = RandomStringUtils.randomAlphabetic(5);
         return genaratedString;
@@ -136,21 +109,18 @@ public class BaseClass {
         return (genaratedString + genaratedNumaber);
     }
 
-    // ---------------- Capture Screenshot ----------------
+    // --------------- Capture Screenshot ---------------
     public String captureScreen(String tname) throws IOException {
 
         String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
-        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
-        File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
 
-        String targetFilePath = System.getProperty("user.dir") + "\\screenshots\\" + tname + "_" + timeStamp + ".png";
-        File targetFile = new File(targetFilePath);
+        String targetPath = System.getProperty("user.dir") + "\\screenshots\\" + tname + "_" + timeStamp + ".png";
+        File target = new File(targetPath);
+        source.renameTo(target);
 
-        // Use FileUtils to reliably copy the file
-        FileUtils.copyFile(sourceFile, targetFile);
-
-        logger.info("Screenshot captured: " + targetFilePath);
-        return targetFilePath;
+        logger.info("Screenshot captured: " + targetPath);
+        return targetPath;
     }
 }
-
